6627a4abf8581c9500fa666567cb5bf0f9c77c4c
JusnSeMiranda <seba-m@hotmail.com>
2021-06-08 15:36:22 -0300

    Se comienza con la implementacion de Alta Asignatura

---
+++ CAltaAsignatura.h
@@ -0,0 +1,13 @@
+#ifndef CALTAASIGNATURA
+#define CALTAASIGNATURA
+#include "ICAltaAsignatura.h"
+
+class CAltaAsignatura : public ICAltaAsignatura{
+    private:
+
+    public:
+        void altaAsignatura();
+        void cancelar();
+};
+
+#endif
---
+++ ICAltaAsignatura.h
@@ -0,0 +1,12 @@
+#ifndef ICALTAASIGNATURA
+#define ICALTAASIGNATURA
+
+class ICAltaAsignatura{
+    public:
+        virtual void altaAsignatura()=0;
+        virtual void cancelar()=0;
+
+
+};
+
+#endif
